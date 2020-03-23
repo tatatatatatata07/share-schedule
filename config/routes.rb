7a4875c0cfc3891cb799d943ef4435a6b89ee2e0
tatatatatatata07 <mptp@hotmail.co.jp>
2020-03-23 07:35:18 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'top_pages#home'
   get '/using',  to: 'top_pages#using'
   get '/signup', to: 'users#new'
-
+  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
