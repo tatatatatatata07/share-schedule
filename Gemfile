eb6c85f2f907780982da81ccbdbf786ebbe32db4
tatatatatatata07 <mptp@hotmail.co.jp>
2020-03-25 04:41:11 +0000

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'carrierwave',             '1.2.2'
 gem 'mini_magick',             '4.7.0'
 gem 'rails-controller-testing'
+gem "jquery-rails"
 
 group :development, :test do
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
-  root 'top_pages#home'
-  get '/using',  to: 'top_pages#using'
-  get '/signup', to: 'users#new'
+  root   'top_pages#home'
+  get    '/using',  to: 'top_pages#using'
+  get    '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
