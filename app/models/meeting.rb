ff0387c26c50e0652d93c48e8df5fb91ca02991f
tatatatatatata07 <mptp@hotmail.co.jp>
2020-04-03 04:09:06 +0000

    complete calender basic test

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,7 +1,7 @@
 class Meeting < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 10 }
+  validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 200 }
   validates :start_time, presence: true
 end
--- test/models/meeting_test.rb
+++ test/models/meeting_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class MeetingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:test_user)
+    @meeting = Meeting.new(title: "Test-title", content: "Test-detail", 
+                     start_time: "2020-04-02 07:47:30" , user_id: @user.id)
+  end
+  
+  test "セットアップしたミーティングデータが有効であることのテスト" do
+    assert @meeting.valid?
+  end
+  
+  test "空白のタイトルは存在できないことのテスト" do
+    @meeting.title = "  "
+    assert_not @meeting.valid?
+  end
+  
+  test "空白のコンテンツは存在できないことのテスト" do
+    @meeting.content = "  "
+    assert_not @meeting.valid?
+  end
+  
+  test "登録時間が空白のミーティングは存在できないことのテスト" do
+    @meeting.start_time = "  "
+    assert_not @meeting.valid?
+  end
+  
+  test "ミーティングのデータはユーザーと紐づいていることをテスト" do
+    @meeting.user_id = "  "
+    assert_not @meeting.valid?
+  end
+
+  
+  test "タイトルは30文字まで有効であることのテスト" do
+    @meeting.title = "a" * 31
+    assert_not @meeting.valid?
+  end
+  
+  test "コンテンツは200文字まで有効であることのテスト" do
+    @meeting.content = "a" * 201
+    assert_not @meeting.valid?
+  end
+  
 end
