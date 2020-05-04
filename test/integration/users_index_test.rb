c2ddb468438e773b0b9859477e9a25d0cab664f7
tatatatatatata07 <mptp@hotmail.co.jp>
2020-05-04 14:21:28 +0000

    created_correct_user_view_test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin     = users(:test_user)
     @non_admin = users(:test_jiro)
+    @not_activation = users(:test_goro)
   end
 
   test "管理者権限アカウントでは削除リンクが表示されること及びユーザー一覧がページネーションされていることをテスト" do
@@ -12,7 +13,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
+    #アクティベーションしていないユーザーは含まれていないことを確認
+    assert_select "a", {count: 0, text: @not_activation.name}
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
