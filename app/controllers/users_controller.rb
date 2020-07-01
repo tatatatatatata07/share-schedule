02141611bb0bd4d25ceee3d0fd238cd718b1d92a
tatatatatatata07 <mptp@hotmail.co.jp>
2020-07-01 06:45:39 +0000

    fix to search function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,12 @@ class UsersController < ApplicationController
   
   def index
     #検索のパラメータ「search」に値が入っていれば検索結果を返し、そうでなければユーザー一覧を表示する
-    if params[:search] != "" or params[:search] != nil
-      @users = User.where(activated: true).where("name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 30)
-      @count = @users.count
-    else
+    if params[:search] == "" or params[:search] == nil
       @users = User.where(activated: true).paginate(page: params[:page], per_page: 30)
       @count = false
+    else
+      @users = User.where(activated: true).where("name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 30)
+      @count = @users.count
     end
     
   end
